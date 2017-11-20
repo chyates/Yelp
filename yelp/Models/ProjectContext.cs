94b0588aa9bdeb41c11ef2b4e4129dd8a1c77ce7
Joshua Daniel <joshuadaniel@fuller.edu>
2017-11-20 14:41:06 -0600

    updated context

--- yelp/Models/ProjectContext.cs
+++ yelp/Models/ProjectContext.cs
@@ -11,8 +11,11 @@ namespace yelp.Models
         public DbSet<Business> Businesses { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<BusCategory> Categories { get; set; }
+        public DbSet<BusCategoryType> CategoryTypes { get; set; }
         public DbSet<BusProperties> Properties { get; set; }
         public DbSet<BusHours> Hours { get; set; }
         public DbSet<ReviewVote> ReviewVotes { get; set; }
+        public DbSet<ReviewImage> ReviewImages { get; set; }
+        public DbSet<UserImage> UserImages { get; set; }
     }
 }
